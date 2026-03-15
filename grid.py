d27fa4000bd5e849f4c361e016853762384cdd5b
FilipK <korlot@kth.se>
2026-03-15 16:47:56 +0100

    clear 62/148 tests

--- grid.py
+++ grid.py
@@ -2,43 +2,62 @@ import sys
 
 data = sys.stdin.read().split()
 grid = []
-current_output = "-1"
-current_depth = 0
+current_depth = -1
 visited = set()
 
+#Add new coordinates if valid for +- active value
 def new_point(row, col):
     _new_list = []
     _current_value = int(grid[row][col])
 
-    #Add new coordinates if valid for +- active value
-    if (row + _current_value) != (row) and (row + _current_value) <= int(data[0]):
-        _new_list.append([str(row + _current_value),str(col)])
+    if (row + _current_value) != (row) and (row + _current_value) < int(data[0]):
+        if ((row + _current_value), col) not in visited:
+            visited.add(((row + _current_value), col))
+            _new_list.append([(row + _current_value),col])
 
     if (row - _current_value) != (row) and (row - _current_value) >= 0:
-        _new_list.append([str(row - _current_value),str(col)])
+        if ((row - _current_value), col) not in visited:
+            visited.add(((row - _current_value), col))
+            _new_list.append([(row - _current_value),col])
 
-    if (col + _current_value) != (col) and (col + _current_value) <= int(data[1]):
-        _new_list.append([str(row), str(col + _current_value)])
+    if (col + _current_value) != (col) and (col + _current_value) < int(data[1]):
+        if (row, (col + _current_value)) not in visited:
+            visited.add((row, (col + _current_value)))
+            _new_list.append([row, (col + _current_value)])
 
     if (col - _current_value) != (col) and (col - _current_value) >= 0:
-        _new_list.append([str(row), str(col - _current_value)])
+        if (row, (col -_current_value)) not in visited:
+            visited.add((row, (col - _current_value)))
+            _new_list.append([row, (col - _current_value)])
+        
     return _new_list
 
 #Move to the next place
-def goto_next(row, col):
-    stack = [[str(row),str(col)]]
-    while stack:
-        active = stack.pop()
-        if active not in visited:
-            visited.add(active)
-            _new_point(active[0], active[1])
-            
+def next_depth(_next):
+    queue = _next
+    stack = []
+    while queue:
+        current_cord = queue.pop()
+        new_cord = new_point(current_cord[0], current_cord[1])
+        for i in range(len(new_cord)):
+            stack.append(new_cord[i])
+    return stack
             
 
-#Seperate row numbers to a list in a list
+#Map out the input to a 2D list.
 for row in range(2, len(data)):
     _temp_data = []
     _target_row = str(data[row])
     for integer in range(len(_target_row)):
         _temp_data.append(_target_row[integer])
     grid.append(_temp_data)
+
+start = [[0,0]]
+
+while (data[0], data[1]) not in visited and start:
+    current_depth +=1
+    start = next_depth(start)
+if ((int(data[0])-1), (int(data[1])-1)) in visited:
+    print(str(current_depth))
+else:
+    print("-1")
